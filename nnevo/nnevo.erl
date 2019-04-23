2503430015a2e61965352f00104377308f6bd099
Alexey Rybakov <rybakov.aax@gmail.com>
2019-04-23 13:00:42 +0300

    mnist_run/0 function

--- nnevo/nnevo.erl
+++ nnevo/nnevo.erl
@@ -12,6 +12,17 @@
 
 %% @doc
 %% Run MNIST tests.
+mnist_run() ->
+
+    % Simple neuronet for digits recognition.
+    Net = nnet:create_multilayer(1, [784, 15, 10]),
+
+    B = parser:mnist_get_binaries("../data/mnist/t10k-images.idx3-ubyte",
+                                  "../data/mnist/t10k-labels.idx1-ubyte"),
+    mnist_run(Net, B, 1).
+
+%% @doc
+%% Run MNIST tests.
 %%   Net - neuronet,
 %%   B - binary data,
 %%   C - current number.
@@ -32,13 +43,7 @@ mnist_run(Net, B, C) ->
 %% @doc
 %% Start function.
 start() ->
-
-    % Simple neuronet for digits recognition.
-    Net = nnet:create_multilayer(1, [784, 15, 10]),
-
-    B = parser:mnist_get_binaries("../data/mnist/t10k-images.idx3-ubyte",
-                                  "../data/mnist/t10k-labels.idx1-ubyte"),
-    mnist_run(Net, B, 1),
+    mnist_run(),
     halt().
 
 %---------------------------------------------------------------------------------------------------
